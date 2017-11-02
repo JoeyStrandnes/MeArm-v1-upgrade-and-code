a5269dbe2c9d05367671ab3e26038fb4d5f28c71
Joey Strandnes <joey.makdesi1@hotmail.com>
2017-11-02 15:36:28 +0100

    Add files via upload

---
+++ Code/Robot_Arm_Wireless_controll_v2.ino
@@ -0,0 +1,52 @@
+//#define BLYNK_PRINT Serial
+
+#include <SPI.h>
+#include <Servo.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "AuthToken"; //Auth token
+char ssid[] = "SSID"; // SSID
+char pass[] = "Paswd"; // Password -WIFI
+
+Servo servoClaw;
+Servo servoLeft;
+Servo servoRight;
+Servo servoBase;
+
+BLYNK_WRITE(V1)
+{
+  servoClaw.write(param.asInt());  
+}
+
+BLYNK_WRITE(V2){
+  servoLeft.write(param.asInt());
+}
+
+BLYNK_WRITE(V3){
+  servoRight.write(param.asInt());
+}
+
+BLYNK_WRITE(V4){
+  servoBase.write(param.asInt());
+}
+
+void setup()
+{
+  
+  //Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+
+  servoClaw.attach(D1);
+  servoLeft.attach(D2);
+  servoRight.attach(D3);
+  servoBase.attach(D4);
+  
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
